32e5321292f4e8416f266bab2e9ee43699fa2553
Dustin Schau <dustinschau@gmail.com>
2018-03-04 20:42:42 -0600

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# gatsby-remark-toc
+
+Adds a table of contents to Markdown files using [mdast-util-toc][mdast-util-toc]
+
+## Install
+
+`npm install gatsby-remark-toc --save`
+
+## How to use
+
+```js
+// in your gatsby-config.js
+plugins: [
+  {
+    resolve: 'gatsby-transformer-remark',
+    options: {
+      plugins: [
+        {
+          resolve: 'gatsby-remark-toc',
+          options: {
+            header: 'Table of Contents', // the custom header text
+            include: [
+              'content/**/*.md' // an include glob to match against
+            ]
+          }
+        }
+      ]
+    }
+  }
+];
+```
+
+[mdast-util-toc]: https://www.npmjs.com/package/mdast-util-toc
---
+++ src/index.js
@@ -0,0 +1,44 @@
+const generateTOC = require('mdast-util-toc');
+const mm = require('micromatch');
+
+module.exports = (
+  { markdownNode, markdownAST },
+  { include = [], header = 'Table of Contents' }
+) => {
+  const filePath = markdownNode.fileAbsolutePath
+    .split(process.cwd())
+    .pop()
+    .replace(/^\//, '');
+  const isIncluded = mm.isMatch(filePath, include);
+
+  if (!isIncluded) {
+    return;
+  }
+
+  const toc = generateTOC(markdownAST).map;
+  const index = markdownAST.children.findIndex(node => node.type !== 'yaml');
+
+  if (!toc || index < 0) {
+    return;
+  }
+
+  const nodes = [
+    header && {
+      type: 'heading',
+      depth: 2,
+      children: [
+        {
+          type: 'text',
+          value: header
+        }
+      ]
+    },
+    toc
+  ].filter(Boolean);
+
+  markdownAST.children = [].concat(
+    markdownAST.children.slice(0, index),
+    ...nodes,
+    markdownAST.children.slice(index)
+  );
+};
